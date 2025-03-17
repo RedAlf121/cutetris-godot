11bd043220053614f32a1585f98e0f634638b35d
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2025-03-17 19:59:46 -0400

    arrow buttons transparent vfx

--- scripts/button_vfx.gd
+++ scripts/button_vfx.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	pass
 
 func _on_Button_pressed():
-	$TextureRect.modulate = Color(1, 1, 1,0.8)  # Cambiar color al presionar
+	$TextureRect.modulate = Color(1, 1, 1,0.5)  # Cambiar color al presionar
 
 func _on_Button_released():
 	$TextureRect.modulate = Color(1, 1, 1,1)  # Restaurar color
