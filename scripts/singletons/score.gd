edc542ec19f50a812e9cde0bfb900ce519cbc10a
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2025-03-19 00:21:26 -0400

    persistence done

--- scripts/singletons/score.gd
+++ scripts/singletons/score.gd
@@ -8,3 +8,12 @@ func increase_score(value: int)->void:
 	score+=value
 	if max_score<=score:
 		max_score=score
+		var config = ConfigFile.new()
+		config.set_value("general","max_score",max_score)
+		config.save("user://settings.cfg")
+
+
+func load_points():
+	var config = Config.new()
+	print(config.get_config("general","max_score",-1))
+	max_score = config.get_config("general","max_score",0)
